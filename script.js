513b1f904117305f65c998499e25af6d2380215b
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-17 16:35:00 +0800

    Add color palette
    Add chart dimensions and spacing

---
+++ script.js
@@ -0,0 +1,10 @@
+// Data Visualization Script
+
+// Set the margin and padding of the SVG
+var margin = { top: 50, right: 20, bottom: 50, left: 100 }
+var padding = 0
+
+// Set the width and height using the current width and height of the div
+var width = 800
+var height = 400
+
