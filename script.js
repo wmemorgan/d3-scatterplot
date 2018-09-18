e7d2da446ec40f6b537efcc61e35948805c5e699
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-18 20:00:43 +0800

    Add new feature to dataset that converts the following fields to Date objects:
    -Seconds
    -Time
    -Year

--- script.js
+++ script.js
@@ -6,28 +6,30 @@ var padding = 50
 
 // Set the width and height using the current width and height of the div
 var width = 800
-var height = 400
+var height = 600
 
 // Get the data
 const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
 const chart = async () => {
   let getData = await fetch(url)
-  let dataset = await getData.json()
-  // console.log(`rawData: `, rawData)
-  // let dataset = rawData.map(d => {
-  //   return {
-  //     Doping: d.Doping, 
-  //     Name: d.Name, 
-  //     Nationality: d.Nationality, 
-  //     Place: d.Place, 
-  //     Seconds: d.Seconds, 
-  //     Time: d.Time, 
-  //     URL: d.URL, 
-  //     Year: d.Year,
-  //     // Year: new Date(Date.parse(d.Year)),
-  //     dopeAllegation: d.Doping.length > 0
-  //   }
-  // })
+  let rawData = await getData.json()
+  console.log(`rawData: `, rawData)
+  let dataset = rawData.map(d => {
+    return {
+      Doping: d.Doping, 
+      Name: d.Name, 
+      Nationality: d.Nationality, 
+      Place: d.Place, 
+      Seconds: d.Seconds, 
+      dateSeconds: new Date(d.Seconds * 1000),
+      Time: d.Time,
+      dateTime: new Date(d.Year + '-01-01T' + '00:'  + d.Time + 'Z'), 
+      URL: d.URL, 
+      Year: d.Year,
+      dateYear: new Date(Date.parse(d.Year)),
+      dopeAllegation: d.Doping.length > 0
+    }
+  })
   console.log(`dataset: `, dataset)
   var noDope = dataset.filter(d => !d.Doping.length > 0)
   console.log(`dataset: (no dope) `, noDope)
@@ -38,10 +40,10 @@ const chart = async () => {
   const maxYear = d3.max(dataset.map(d => d.Year + 1))
   console.log(`maxYear: ${maxYear}`)
 
-  const minSeconds = d3.min(dataset.map(d => d.Seconds))
-  const maxSeconds = d3.max(dataset.map(d => d.Seconds))
+  const minSeconds = d3.min(dataset.map(d => d.dateSeconds))
+  const maxSeconds = d3.max(dataset.map(d => d.dateSeconds))
 
-  // Set the ranges
+  // Define scale
   const xScale = d3.scaleTime()
     .domain([minYear, maxYear])
     .range([padding, width - padding])
@@ -50,45 +52,45 @@ const chart = async () => {
     .domain([minSeconds, maxSeconds])
     .range([padding, (height - padding)])
 
-  const yAxisScale = d3.scaleTime()
-    .domain([minSeconds * 1000, maxSeconds * 1000])
-    .range([padding, (height - padding)])
+  // const yAxisScale = d3.scaleTime()
+  //   .domain([minSeconds * 1000, maxSeconds * 1000])
+  //   .range([padding, (height - padding)])
 
-  // create svg and append to chart div
+  // Create svg and append to chart div
   var svg = d3.select('#chart')
     .append('svg')
     .attr('class', 'graph')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
 
-  // add labels 
-  // title
+  // Add labels 
+  // Title
   svg.append('text')
     .text('Doping in Professional Bicycle Racing')
     .attr('id', 'title')
     .attr("x", width / 2)
     .attr("y", padding / 2) 
   
-  // axis labels
+  // Axes Labels
   svg.append('text')
     .attr('transform', 'rotate(-90)')
     .attr('x', -250)
     .attr('y', 5)
-    // .attr('class', 'labels')
+    .attr('class', 'labels')
     .text('Time in Minutes')
 
   svg.append('text')
     .attr('x', width / 2)
     .attr('y', height)
-    // .attr('class', 'labels')
+    .attr('class', 'labels')
     .text('Year')
 
-  // tooltip  
+  // Tooltip  
   const tooltip = d3.select('#chart').append('div')
     .attr('id', 'tooltip')
     .style('opacity', 0)
 
- // legend code inspired by Hooria Hic
+ // Legend code inspired by Hooria Hic
  // https://codepen.io/HIC/full/NLLmPp/
   const legend = svg.append('g')
     .attr('id', 'legend')
@@ -120,11 +122,10 @@ const chart = async () => {
     .attr("font-size", "1em")
     .text(d => d.text);
 
-  // add the x Axis 
+  // Add axes
   const xAxis = d3.axisBottom(xScale)
     .tickFormat(d3.format("d"))
-
-  const yAxis = d3.axisLeft(yAxisScale)
+  const yAxis = d3.axisLeft(yScale)
     .tickFormat(d3.scaleTime().tickFormat(10, "%M:%S"))
 
   svg.append('g')
@@ -133,21 +134,21 @@ const chart = async () => {
     .attr('class', 'axis')
     .attr('transform', `translate(0, ${height - padding})`)
 
-  // add the y Axis
   svg.append('g')
     .call(yAxis)
     .attr('id', 'y-axis')
     .attr('class', 'axis')
     .attr('transform', `translate(${padding}, 0)`)
 
+  // Add scatterplot  
   svg.selectAll("circle")
     .data(dataset)
     .enter()
     .append("circle")
     .attr('data-xvalue', (d) => d.Year)
-    .attr('data-yvalue', (d) => d.Seconds)
+    .attr('data-yvalue', (d) => d.dateSeconds)
     .attr("cx", (d) => xScale(d.Year))
-    .attr("cy", (d) => yScale(d.Seconds))
+    .attr("cy", (d) => yScale(d.dateSeconds))
     .attr("r", 8)
     .attr('class', (d) => d.Doping.length > 0 ? 'doping' : 'not-doping')
     .classed('dot', true)
