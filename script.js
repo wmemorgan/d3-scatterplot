ec224706aae47ddfb6aa169b2e63b9e1d24de9a2
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-18 16:47:15 +0800

    Remove data feature engineering
    Refactor xScale
    
    Add yAxis scale for y-axis
    
    Refactor dope allegation flag

--- script.js
+++ script.js
@@ -12,42 +12,47 @@ var height = 400
 const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
 const chart = async () => {
   let getData = await fetch(url)
-  let rawData = await getData.json()
-  console.log(`rawData: `, rawData)
-  let dataset = rawData.map(d => {
-    return {
-      Doping: d.Doping, 
-      Name: d.Name, 
-      Nationality: d.Nationality, 
-      Place: d.Place, 
-      Seconds: d.Seconds, 
-      Time: d.Time, 
-      URL: d.URL, 
-      Year: new Date(Date.parse(d.Year)),
-      dopeAllegation: d.Doping.length > 0
-    }
-  })
+  let dataset = await getData.json()
+  // console.log(`rawData: `, rawData)
+  // let dataset = rawData.map(d => {
+  //   return {
+  //     Doping: d.Doping, 
+  //     Name: d.Name, 
+  //     Nationality: d.Nationality, 
+  //     Place: d.Place, 
+  //     Seconds: d.Seconds, 
+  //     Time: d.Time, 
+  //     URL: d.URL, 
+  //     Year: d.Year,
+  //     // Year: new Date(Date.parse(d.Year)),
+  //     dopeAllegation: d.Doping.length > 0
+  //   }
+  // })
   console.log(`dataset: `, dataset)
-  var noDope = dataset.filter(d => !d.dopeAllegation)
+  var noDope = dataset.filter(d => !d.Doping.length > 0)
   console.log(`dataset: (no dope) `, noDope)
   // Format the data
   // Year
-  // const minYear = d3.min(dataset.map(d => d.Year))
-  // console.log(`minYear: ${minYear}`)
-  // const maxYear = d3.max(dataset.map(d => d.Year))
-  // console.log(`maxYear: ${maxYear}`)
+  const minYear = d3.min(dataset.map(d => d.Year - 1))
+  console.log(`minYear: ${minYear}`)
+  const maxYear = d3.max(dataset.map(d => d.Year))
+  console.log(`maxYear: ${maxYear}`)
 
   const minSeconds = d3.min(dataset.map(d => d.Seconds))
   const maxSeconds = d3.max(dataset.map(d => d.Seconds))
 
   // Set the ranges
   const xScale = d3.scaleTime()
-  .domain(d3.extent(dataset, (d) => d.Year))
-  .range([padding, width-padding])
+    .domain([minYear, maxYear])
+    .range([padding, width - padding])
 
   const yScale = d3.scaleTime()
     .domain([minSeconds, maxSeconds])
-    .range([padding, (height-padding)])
+    .range([padding, (height - padding)])
+
+  const yAxisScale = d3.scaleTime()
+    .domain([minSeconds * 1000, maxSeconds * 1000])
+    .range([padding, (height - padding)])
 
   // create svg and append to chart div
   var svg = d3.select('#chart')
@@ -58,14 +63,15 @@ const chart = async () => {
 
   // add the x Axis 
   const xAxis = d3.axisBottom(xScale)
+    .tickFormat(d3.format("d"))
 
-  const yAxis = d3.axisLeft(yScale)
+  const yAxis = d3.axisLeft(yAxisScale)
     .tickFormat(d3.scaleTime().tickFormat(10, "%M:%S"));
 
   svg.append('g')
     .call(xAxis)
     .attr('id', 'x-axis')
-    .attr('transform', `translate(0, ${height})`)
+    .attr('transform', `translate(0, ${height - padding})`)
 
   // add the y Axis
   svg.append('g')
@@ -89,7 +95,7 @@ const chart = async () => {
     .attr("cy", (d) => yScale(d.Seconds))
     .attr("r", 8)
     .attr('class', 'dot')
-    .style('fill', (d) => d.dopeAllegation ? 'red' : 'green')
+    .style('fill', (d) => d.Doping.length > 0 ? 'red' : 'green')
 }
 
 chart()
